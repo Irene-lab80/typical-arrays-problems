eae01176494830e23d47456ab692ab9ddd7118e0
IrinaZ <you@example.com>
2020-10-03 16:43:59 +0300

    feat: implemented three functions in typical arrays

--- src/index.js
+++ src/index.js
@@ -1,12 +1,45 @@
 
 exports.min = function min (array) {
-  return 0;
+    var min_v = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] <= min_v) {
+            min_v = array[i];
+        }   
+    }
+    return min_v
 }
 
+/*(array === undefined || array.length === 0)*/
+
 exports.max = function max (array) {
-  return 0;
+    var max_v = array[0];
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] >= max_v) {
+            max_v = array[i];
+        }   
+    }
+    return max_v
 }
 
 exports.avg = function avg (array) {
-  return 0;
-}
+    let count = 0;
+    let total = 0;
+    for(let i = 0; i < array.length; i++) {
+        if(array[i] === undefined || array.length === 0) {
+            avg = 0;
+        } else {count++; total += array[i];}
+
+    }
+    let avg = total / count;
+    return avg;
+}    
+
+
+  
+  
+    /*if (array === undefined || array[0] === undefined) {
+    return 0; 
+  }*/
+
+
+
